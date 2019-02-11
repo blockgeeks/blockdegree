08039e8bdb865f2957023678f76a1346a1a5cbb5
SashaBG <31702239+sashabaksht@users.noreply.github.com>
2019-02-11 12:37:08 -0500

    Create proxy.sol

---
+++ proxy.sol
@@ -0,0 +1,15 @@
+// implement a proxy contract that accepts any function call and forwards it to a target contract.
+// The function signatures/paramaters needed are provided but you will need to create your own state variables
+pragma solidity ^0.4.24;
+
+contract Proxy {
+
+  constructor(address _target) {
+  }
+
+  function() {
+  }
+
+  function execute() returns (bool) {
+  }
+}
